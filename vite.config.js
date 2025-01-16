9c52ea15ef8122126bb685778fec1b2347dfa0d3
Adrien Poly <adrienpoly@gmail.com>
2025-01-16 10:28:40 +0100

    add github page

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 export default {
+    base: '/tip_tap_labels/',
     server: {
         port: 3000,
         open: true
+    },
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets'
     }
 }
